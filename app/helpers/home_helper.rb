c46d5fefb71cccea7402183b0af8e7cd58eee2ef
Evan Medrano <evan.a.medrano@gmail.com>
2020-07-06 09:37:25 -0400

    Remove header and adjust styles for home page

--- app/helpers/home_helper.rb
+++ app/helpers/home_helper.rb
@@ -1,2 +1,9 @@
 module HomeHelper
+  def sign_up_button_classes
+    "button button--large button--red d-block d-sm-inline-block my-4 my-sm-0"
+  end
+
+  def log_in_button_classes
+    "button button--large button--transparent d-block d-sm-inline-block ml-sm-4"
+  end
 end
