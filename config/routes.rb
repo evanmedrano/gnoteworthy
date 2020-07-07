f430e76aa3b192f4a6a6ddc2247a0c9c288a1167
Evan Medrano <evan.a.medrano@gmail.com>
2020-07-07 08:29:07 -0400

    Refactor devise forms scss files

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   root to: 'home#index'
 
   devise_for :users
-  get '/user' => 'dashboards#show', :as => :user_root
+  get '/dashboard' => 'dashboards#show', :as => :user_root
 
   resource :dashboard, only: [:show]
 end
